b4101c8e774f6cd3cee11a87426ec43b71fec40e
Sílvia Agrassot <silvia.agrassot@processcontrol.com>
2026-05-08 17:34:12 +0200

    Exercici ej1c3.py Acabat

--- 1c/ej1c3.py
+++ 1c/ej1c3.py
@@ -38,11 +38,19 @@ Exemple:
 
 
 def find_max(lst):
-    # Write here your code
+    # Cas Base: que la llista sigui buida o només hi hagi un element
+    if len(lst) == 1:
+        return lst[0]
+    # Recursió
+    ValorMaxim = find_max(lst[1:])
+    if lst[0] > ValorMaxim:
+        return lst[0]
+    else:
+        return ValorMaxim
     pass
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# numbers_list = [1, 5, 2, 7, 3]
-# print(find_max(numbers_list))
+numbers_list = [1, 5, 2, 7, 3]
+print(find_max(numbers_list))
